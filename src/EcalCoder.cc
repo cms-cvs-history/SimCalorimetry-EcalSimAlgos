c4f50cd35cde7288cf3c8d85aa61b86c82827169
cmsbuild <>
2010-03-28 08:24:33 +0000

    This commit was manufactured by cvs2git to create tag 'SimCalorimetry- EcalSimAlgos-V02-02-03'.
    Sprout from master 2010-03-28 08:24:32 UTC Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch> 'Make ES changes work in 360_pre2'
    Cherrypick from master 2009-10-13 15:19:47 UTC Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch> 'Restore previous EcalCoder to HEAD':
        interface/EcalCoder.h
        interface/EcalCorrelatedNoiseMatrix.h
        interface/EcalTDigitizer.h
        src/EcalCoder.cc
        test/testEcalShape.cpp
    Delete:
        interface/APDShape.h
        interface/APDSimParameters.h
        interface/EBHitResponse.h
        interface/EcalTDigitizer.icc
        src/APDShape.cc
        src/EBHitResponse.cc
        src/EcalDigitizers.cc

--- src/EcalCoder.cc
+++ src/EcalCoder.cc
@@ -1,278 +1,234 @@
 #include "SimCalorimetry/EcalSimAlgos/interface/EcalCoder.h"
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include "SimGeneral/NoiseGenerators/interface/CorrelatedNoisifier.h"
 #include "DataFormats/EcalDigi/interface/EcalMGPASample.h"
-#include "DataFormats/EcalDigi/interface/EcalDataFrame.h"
-//#include "CLHEP/Random/RandGaussQ.h"
+#include "DataFormats/EcalDigi/interface/EBDataFrame.h"
+#include "DataFormats/EcalDigi/interface/EEDataFrame.h"
+#include "CLHEP/Random/RandGaussQ.h"
 #include <iostream>
 
-EcalCoder::EcalCoder( bool                  addNoise     , 
-		      EcalCoder::Noisifier* ebCorrNoise0 ,
-		      EcalCoder::Noisifier* eeCorrNoise0 ,
-		      EcalCoder::Noisifier* ebCorrNoise1 ,
-		      EcalCoder::Noisifier* eeCorrNoise1 ,
-		      EcalCoder::Noisifier* ebCorrNoise2 ,
-		      EcalCoder::Noisifier* eeCorrNoise2   ) :
-   m_peds        (           0 ) ,
-   m_gainRatios  (           0 ) ,
-   m_intercals   (           0 ) ,
-   m_maxEneEB    (      1668.3 ) , // 4095(MAXADC)*12(gain 2)*0.035(GeVtoADC)*0.97
-   m_maxEneEE    (      2859.9 ) , // 4095(MAXADC)*12(gain 2)*0.060(GeVtoADC)*0.97
-   m_addNoise    ( addNoise    )
+
+EcalCoder::EcalCoder(bool addNoise, CorrelatedNoisifier<EcalCorrMatrix> * theCorrNoise)
+:  thePedestals(0),
+   addNoise_(addNoise),
+   theCorrNoise_(theCorrNoise)
+
 {
-   m_ebCorrNoise[0] = ebCorrNoise0 ;
-   assert( 0 != m_ebCorrNoise[0] ) ;
-   m_eeCorrNoise[0] = eeCorrNoise0 ;
-   m_ebCorrNoise[1] = ebCorrNoise1 ;
-   m_eeCorrNoise[1] = eeCorrNoise1 ;
-   m_ebCorrNoise[2] = ebCorrNoise2 ;
-   m_eeCorrNoise[2] = eeCorrNoise2 ;
+
+  // 4095(MAXADC)*12(gain 2)*0.035(GeVtoADC)*0.97
+  
+  m_maxEneEB = 1668.3 ; 
+  
+  // 4095(MAXADC)*12(gain 2)*0.060(GeVtoADC)*0.97
+  
+  m_maxEneEE = 2859.9 ; 
+  
 }  
 
-EcalCoder::~EcalCoder()
-{
+void  EcalCoder::setPedestals(const EcalPedestals * pedestals) {
+  thePedestals = pedestals;
 }
 
-void 
-EcalCoder::setFullScaleEnergy( double EBscale ,
-			       double EEscale   )
-{
-   m_maxEneEB = EBscale ;
-   m_maxEneEE = EEscale ;
+void  EcalCoder::setGainRatios(const EcalGainRatios * gainRatios) {
+  theGainRatios = gainRatios; 
 }
 
+void EcalCoder::setIntercalibConstants(const EcalIntercalibConstantsMC * ical) {
+  theIntercalibConstants = ical;
+} 
 
-void  
-EcalCoder::setPedestals( const EcalPedestals* pedestals ) 
+double EcalCoder::fullScaleEnergy(const DetId & detId) const 
 {
-   m_peds = pedestals ;
-}
 
-void  
-EcalCoder::setGainRatios( const EcalGainRatios* gainRatios ) 
-{
-   m_gainRatios = gainRatios ; 
-}
-
-void 
-EcalCoder::setIntercalibConstants( const EcalIntercalibConstantsMC* ical ) 
-{
-   m_intercals = ical ;
-}
-
-double 
-EcalCoder::fullScaleEnergy( const DetId & detId ) const 
-{
-   return detId.subdetId() == EcalBarrel ? m_maxEneEB : m_maxEneEE ;
-}
-
-void 
-EcalCoder::analogToDigital( const CaloSamples& clf , 
-			    EcalDataFrame&     df    ) const 
-{
-   df.setSize( clf.size() ) ;
-   encode( clf, df );
-}
-
-void 
-EcalCoder::encode( const CaloSamples& caloSamples , 
-		   EcalDataFrame&     df            ) const
-{
-   assert( 0 != m_peds ) ;
-
-   const unsigned int csize ( caloSamples.size() ) ;
-
+  if (detId.subdetId() == EcalBarrel) 
+    return m_maxEneEB ;
+  else 
+    return m_maxEneEE ;
   
-   DetId detId = caloSamples.id();             
-   double Emax = fullScaleEnergy(detId);       
+}
 
-   //....initialisation
-   if ( caloSamples[5] > 0. ) LogDebug("EcalCoder") << "Input caloSample" << "\n" << caloSamples;
+
+void EcalCoder::digitalToAnalog(const EBDataFrame& df, CaloSamples& lf) const {
+  for(int i = 0; i < df.size(); ++i) {
+    lf[i] = decode(df[i], df.id());
+  }
+}  
+
+void EcalCoder::digitalToAnalog(const EEDataFrame& df, CaloSamples& lf) const {
+  for(int i = 0; i < df.size(); ++i) {
+    lf[i] = decode(df[i], df.id());
+  }
+}
+
+void EcalCoder::analogToDigital(const CaloSamples& clf, EcalDataFrame& df) const {
+
+  df.setSize(clf.size());
+  encode(clf, df);
+}
+
+void EcalCoder::encode(const CaloSamples& caloSamples, EcalDataFrame& df) const
+{
+  assert(thePedestals != 0);
   
-   double LSB[NGAINS+1];
-   double pedestals[NGAINS+1];
-   double widths[NGAINS+1];
-   double gains[NGAINS+1];
-   int    maxADC[NGAINS+1];
-   double trueRMS[NGAINS+1];
+  DetId detId = caloSamples.id();             
+  double Emax = fullScaleEnergy(detId);       
 
-   double icalconst = 1. ;
-   findIntercalibConstant( detId, icalconst );
+  //....initialisation
+  if ( caloSamples[5] > 0. ) 
+    LogDebug("EcalCoder") << "Input caloSample" << "\n" << caloSamples;
+  
+  double LSB[NGAINS+1];
+  double pedestals[NGAINS+1];
+  double widths[NGAINS+1];
+  double gains[NGAINS+1];
+  double threeSigmaADCNoise[NGAINS+1];
+  int    maxADC[NGAINS+1];
 
-   for( unsigned int igain ( 0 ); igain <= NGAINS ; ++igain ) 
-   {
-      // fill in the pedestal and width
-      findPedestal( detId ,
-		    igain , 
-		    pedestals[igain] , 
-		    widths[igain]      ) ;
+  double icalconst = 1. ;
+  findIntercalibConstant(detId, icalconst);
 
-      if( 0 < igain ) 
-	 trueRMS[igain] = std::sqrt( widths[igain]*widths[igain] - 1./12. ) ;
+  for(int igain = 0; igain <= NGAINS; ++igain) {
 
-      // set nominal value first
-      findGains( detId , 
-		 gains  );               
+    // fill in the pedestal and width
+    findPedestal(detId, igain, pedestals[igain], widths[igain]);
 
-      LSB[igain] = 0.;
-      if ( igain > 0 ) LSB[igain]= Emax/(MAXADC*gains[igain]);
-      maxADC[igain] = ADCGAINSWITCH;                   // saturation at 4080 for middle and high gains x6 & x12
-      if ( igain == NGAINS ) maxADC[igain] = MAXADC;   // saturation at 4095 for low gain x1 
-   }
+    // set nominal value first
+    findGains(detId, gains);               
 
-   CaloSamples noiseframe[] = { CaloSamples( detId , csize ) ,
-				CaloSamples( detId , csize ) ,
-				CaloSamples( detId , csize )   } ;
+    LSB[igain] = 0.;
+    if ( igain > 0 ) LSB[igain]= Emax/(MAXADC*gains[igain]);
+    threeSigmaADCNoise[igain] = 0.;
+    if ( igain > 0 ) threeSigmaADCNoise[igain] = widths[igain] * 3.;
+    maxADC[igain] = ADCGAINSWITCH;                   // saturation at 4080 for middle and high gains x6 & x12
+    if ( igain == NGAINS ) maxADC[igain] = MAXADC;   // saturation at 4095 for low gain x1 
+  }
 
-   const Noisifier* noisy[3] = { ( 0 == m_eeCorrNoise[0]          ||
-				   EcalBarrel == detId.subdetId()    ?
-				   m_ebCorrNoise[0] :
-				   m_eeCorrNoise[0]                  ) ,
-				 ( EcalBarrel == detId.subdetId() ?
-				   m_ebCorrNoise[1] :
-				   m_eeCorrNoise[1]                  ) ,
-				 ( EcalBarrel == detId.subdetId() ?
-				   m_ebCorrNoise[2] :
-				   m_eeCorrNoise[2]                  )   } ;
+  CaloSamples noiseframe(detId, caloSamples.size());        
+  if (addNoise_) { 
+    theCorrNoise_->noisify(noiseframe);
+    LogDebug("EcalCoder") << "Normalized correlated noise calo frame = " << noiseframe;
+  }
 
-   if( m_addNoise )
-   {
-      noisy[0]->noisify( noiseframe[0] ) ; // high gain
-      if( 0 == noisy[1] ) noisy[0]->noisify( noiseframe[1] ,
-					     &noisy[0]->vecgau() ) ; // med 
-      if( 0 == noisy[2] ) noisy[0]->noisify( noiseframe[2] ,
-					     &noisy[0]->vecgau() ) ; // low
-   }
+  int wait = 0 ;
+  int gainId = 0 ;
+  bool isSaturated = 0;
+  for (int i = 0 ; i < caloSamples.size() ; ++i)
+  {    
+     int adc = MAXADC;
+     if (wait == 0) gainId = 1;
 
-   int wait = 0 ;
-   int gainId = 0 ;
-   bool isSaturated = 0;
+     // see which gain bin it fits in
+     int igain = gainId-1 ;
+     while (igain != 3) {
+       ++igain;
 
-   for( unsigned int i ( 0 ) ; i != csize ; ++i )
-   {    
-      bool done ( false ) ;
-      int adc = MAXADC ;
-      if( 0 == wait ) gainId = 1 ;
+       double ped = pedestals[igain];
+       double signal = ped + caloSamples[i] / LSB[igain] / icalconst;
 
-      // see which gain bin it fits in
-      int igain ( gainId - 1 ) ;
-      do
-      {
-	 ++igain ;
-
-	 if( igain != gainId ) std::cout <<"$$$$ Gain switch from " << gainId
-					 <<" to "<< igain << std::endl ;
-
-	 if( 1 != igain                    &&   // not high gain
-	     m_addNoise                    &&   // want to add noise
-	     0 != noisy[igain-1]           &&   // exists
-	     noiseframe[igain-1].isBlank()    ) // not already done
-	 {
-	    noisy[igain-1]->noisify( noiseframe[igain-1] ,
-				     &noisy[0]->vecgau()   ) ;
-	    std::cout<<"....noisifying gain level = "<<igain<<std::endl ;
-	 }
-	
-	 // noiseframe filled with zeros if !m_addNoise
-	 const double signal ( pedestals[igain] +
-			       caloSamples[i] /( LSB[igain]*icalconst ) +
-			       trueRMS[igain]*noiseframe[igain-1][i]      ) ;
-	 
-	 const int isignal ( signal ) ;
-	 const int tmpadc ( signal - (double)isignal < 0.5 ?
-			    isignal : isignal + 1 ) ;
-	 // LogDebug("EcalCoder") << "DetId " << detId.rawId() << " gain " << igain << " caloSample " 
-	 //                       <<  caloSamples[i] << " pededstal " << pedestals[igain] 
-	 //                       << " noise " << widths[igain] << " conversion factor " << LSB[igain] 
-	 //                       << " result (ped,tmpadc)= " << ped << " " << tmpadc;
+       // see if it's close enough to the boundary that we have to throw noise
+       if(addNoise_ && (signal <= maxADC[igain]+threeSigmaADCNoise[igain]) ) {
+         // width is the actual final noise, subtract the additional one from the trivial quantization
+         double trueRMS = std::sqrt(widths[igain]*widths[igain]-1./12.);
+         ped = ped + trueRMS*noiseframe[i];
+         signal = ped + caloSamples[i] / LSB[igain] / icalconst;
+       }
+       int tmpadc = (signal-(int)signal <= 0.5 ? (int)signal : (int)signal + 1);
+       // LogDebug("EcalCoder") << "DetId " << detId.rawId() << " gain " << igain << " caloSample " 
+       //                       <<  caloSamples[i] << " pededstal " << pedestals[igain] 
+       //                       << " noise " << widths[igain] << " conversion factor " << LSB[igain] 
+       //                       << " result (ped,tmpadc)= " << ped << " " << tmpadc;
          
-	 if( tmpadc <= maxADC[igain] ) 
-	 {
-	    adc = tmpadc;
-	    done = true ;
-	 }
-      }
-      while( !done       &&
-	     igain < 3    ) ;
-
-      if (igain == 1 ) 
-      {
+       if(tmpadc <= maxADC[igain] ) {
+         adc = tmpadc;
+         break ;
+       }
+     }
+     
+     if (igain == 1) 
+       {
          wait = 0 ;
          gainId = igain ;
-      }
-      else 
-      {
+       }
+     else 
+       {
          if (igain == gainId) --wait ;
          else 
-	 {
-	    wait = 5 ;
-	    gainId = igain ;
-	 }
-      }
+           {
+             wait = 5 ;
+             gainId = igain ;
+           }
+       }
 
 
-      // change the gain for saturation
-      int storeGainId = gainId;
-      if ( gainId == 3 && adc == MAXADC ) 
-      {
-	 storeGainId = 0;
-	 isSaturated = true;
-      }
-      // LogDebug("EcalCoder") << " Writing out frame " << i << " ADC = " << adc << " gainId = " << gainId << " storeGainId = " << storeGainId ; 
+     // change the gain for saturation
+     int storeGainId = gainId;
+     if ( gainId == 3 && adc == MAXADC ) {
+             storeGainId = 0;
+             isSaturated = true;
+     }
+     // LogDebug("EcalCoder") << " Writing out frame " << i << " ADC = " << adc << " gainId = " << gainId << " storeGainId = " << storeGainId ; 
      
-      df.setSample( i, EcalMGPASample( adc, storeGainId ) );   
-   }
-   // handle saturation properly according to IN-2007/056
-   // N.B. - FIXME 
-   // still missing the possibility for a signal to pass the saturation threshold
-   // again within the 5 subsequent samples (higher order effect).
-
-   if ( isSaturated ) 
-   {
-      for (int i = 0 ; i < caloSamples.size() ; ++i) 
-      {
-	 if ( df.sample(i).gainId() == 0 ) 
-	 {
-	    int hyst = i+1+2;
-	    for ( int j = i+1; j < hyst && j < caloSamples.size(); ++j ) 
-	    {
-	       df.setSample(j, EcalMGPASample(MAXADC, 0));   
-	    }
-	 }
-      }
-   }
+     df.setSample(i, EcalMGPASample(adc, storeGainId));   
+  }
+  // handle saturation properly according to IN-2007/056
+  // N.B. - FIXME 
+  // still missing the possibility for a signal to pass the saturation threshold
+  // again within the 5 subsequent samples (higher order effect).
+  if ( isSaturated ) {
+          for (int i = 0 ; i < caloSamples.size() ; ++i) {
+                  if ( df.sample(i).gainId() == 0 ) {
+                          int hyst = i+1+2;
+                          for ( int j = i+1; j < hyst && j < caloSamples.size(); ++j ) {
+                                  df.setSample(j, EcalMGPASample(MAXADC, 0));   
+                          }
+                  }
+          }
+  }
 }
 
-void 
-EcalCoder::findPedestal( const DetId & detId  , 
-			 int           gainId , 
-			 double&       ped    , 
-			 double&       width     ) const
+double EcalCoder::decode(const EcalMGPASample & sample, const DetId & id) const
 {
-   /*
-     EcalPedestalsMapIterator mapItr 
-     = m_peds->m_pedestals.find(detId.rawId());
-     // should I care if it doesn't get found?
-     if(mapItr == m_peds->m_pedestals.end()) {
-     edm::LogError("EcalCoder") << "Could not find pedestal for " << detId.rawId() << " among the " << m_peds->m_pedestals.size();
-     } else {
-     EcalPedestals::Item item = mapItr->second;
-   */
-   
-   /*   
-	EcalPedestals::Item const & item = (*m_peds)(detId);
-	ped = item.mean(gainId);
-	width = item.rms(gainId);
-   */
+  double Emax = fullScaleEnergy(id); 
+  int gainNumber  = sample.gainId();
+  if(gainNumber==0){gainNumber=3;}
+  assert( gainNumber >=1 && gainNumber <=3);
+  double gains[NGAINS+1];
+  findGains(id, gains);
+  double LSB = Emax/(MAXADC*gains[gainNumber]) ;
+  double pedestal = 0.;
+  double width = 0.;
+  findPedestal(id, gainNumber, pedestal, width);
+  double icalconst = 1. ;
+  findIntercalibConstant(id, icalconst);
+  // we shift by LSB/2 to be centered
+  return icalconst * LSB * (sample.adc() + 0.5 - pedestal) ;
+}
 
-   EcalPedestalsMap::const_iterator itped = m_peds->getMap().find( detId );
-   ped   = (*itped).mean(gainId);
-   width = (*itped).rms(gainId);
+
+void EcalCoder::findPedestal(const DetId & detId, int gainId, 
+			     double & ped, double & width) const
+{
+  /*
+    EcalPedestalsMapIterator mapItr 
+    = thePedestals->m_pedestals.find(detId.rawId());
+    // should I care if it doesn't get found?
+    if(mapItr == thePedestals->m_pedestals.end()) {
+    edm::LogError("EcalCoder") << "Could not find pedestal for " << detId.rawId() << " among the " << thePedestals->m_pedestals.size();
+    } else {
+    EcalPedestals::Item item = mapItr->second;
+  */
+
+  /*   
+    EcalPedestals::Item const & item = (*thePedestals)(detId);
+    ped = item.mean(gainId);
+    width = item.rms(gainId);
+  */
+
+  EcalPedestalsMap::const_iterator itped = thePedestals->getMap().find( detId );
+  ped   = (*itped).mean(gainId);
+  width = (*itped).rms(gainId);
   
-   if ( (detId.subdetId() != EcalBarrel) && (detId.subdetId() != EcalEndcap) ) 
-   { 
-      edm::LogError("EcalCoder") << "Could not find pedestal for " << detId.rawId() << " among the " << m_peds->getMap().size();
-   } 
+  if ( (detId.subdetId() != EcalBarrel) && (detId.subdetId() != EcalEndcap) ) { 
+    edm::LogError("EcalCoder") << "Could not find pedestal for " << detId.rawId() << " among the " << thePedestals->getMap().size();
+  } 
 
   /*
     switch(gainId) {
@@ -297,18 +253,16 @@ EcalCoder::findPedestal( const DetId & detId  ,
     }
   */
 
-   LogDebug("EcalCoder") << "Pedestals for " << detId.rawId() << " gain range " << gainId << " : \n" << "Mean = " << ped << " rms = " << width;
+  LogDebug("EcalCoder") << "Pedestals for " << detId.rawId() << " gain range " << gainId << " : \n" << "Mean = " << ped << " rms = " << width;
 }
 
 
-void 
-EcalCoder::findGains( const DetId & detId , 
-		      double Gains[]        ) const
+void EcalCoder::findGains(const DetId & detId, double Gains[]) const
 {
   /*
-    EcalGainRatios::EcalGainRatioMap::const_iterator grit=m_gainRatios->getMap().find(detId.rawId());
+    EcalGainRatios::EcalGainRatioMap::const_iterator grit=theGainRatios->getMap().find(detId.rawId());
     EcalMGPAGainRatio mgpa;
-    if( grit!=m_gainRatios->getMap().end() ){
+    if( grit!=theGainRatios->getMap().end() ){
     mgpa = grit->second;
     Gains[0] = 0.;
     Gains[3] = 1.;
@@ -316,40 +270,34 @@ EcalCoder::findGains( const DetId & detId ,
     Gains[1] = Gains[2]*(mgpa.gain12Over6()) ;
     LogDebug("EcalCoder") << "Gains for " << detId.rawId() << "\n" << " 1 = " << Gains[1] << "\n" << " 2 = " << Gains[2] << "\n" << " 3 = " << Gains[3];
     } else {
-    edm::LogError("EcalCoder") << "Could not find gain ratios for " << detId.rawId() << " among the " << m_gainRatios->getMap().size();
+    edm::LogError("EcalCoder") << "Could not find gain ratios for " << detId.rawId() << " among the " << theGainRatios->getMap().size();
     }
   */
 
-   EcalGainRatioMap::const_iterator grit = m_gainRatios->getMap().find( detId );
-   Gains[0] = 0.;
-   Gains[3] = 1.;
-   Gains[2] = (*grit).gain6Over1();
-   Gains[1] = Gains[2]*( (*grit).gain12Over6() );   
-   
+  EcalGainRatioMap::const_iterator grit = theGainRatios->getMap().find( detId );
+  Gains[0] = 0.;
+  Gains[3] = 1.;
+  Gains[2] = (*grit).gain6Over1();
+  Gains[1] = Gains[2]*( (*grit).gain12Over6() );   
+
   
-   if ( (detId.subdetId() != EcalBarrel) && (detId.subdetId() != EcalEndcap) ) 
-   { 
-      edm::LogError("EcalCoder") << "Could not find gain ratios for " << detId.rawId() << " among the " << m_gainRatios->getMap().size();
-   }   
+  if ( (detId.subdetId() != EcalBarrel) && (detId.subdetId() != EcalEndcap) ) { 
+    edm::LogError("EcalCoder") << "Could not find gain ratios for " << detId.rawId() << " among the " << theGainRatios->getMap().size();
+  }   
   
 }
 
-void 
-EcalCoder::findIntercalibConstant( const DetId& detId, 
-				   double&      icalconst ) const
+void EcalCoder::findIntercalibConstant(const DetId & detId, double & icalconst) const
 {
-   EcalIntercalibConstantMC thisconst = 1.;
-   // find intercalib constant for this xtal
-   const EcalIntercalibConstantMCMap &icalMap = m_intercals->getMap();
-   EcalIntercalibConstantMCMap::const_iterator icalit = icalMap.find(detId);
-   if( icalit!=icalMap.end() )
-   {
-      thisconst = (*icalit);
-      if ( icalconst == 0. ) { thisconst = 1.; }
-   } 
-   else
-   {
-      edm::LogError("EcalCoder") << "No intercalib const found for xtal " << detId.rawId() << "! something wrong with EcalIntercalibConstants in your DB? ";
-   }
-   icalconst = thisconst;
+  EcalIntercalibConstantMC thisconst = 1.;
+  // find intercalib constant for this xtal
+  const EcalIntercalibConstantMCMap &icalMap = theIntercalibConstants->getMap();
+  EcalIntercalibConstantMCMap::const_iterator icalit = icalMap.find(detId);
+  if( icalit!=icalMap.end() ){
+    thisconst = (*icalit);
+    if ( icalconst == 0. ) { thisconst = 1.; }
+  } else {
+    edm::LogError("EcalCoder") << "No intercalib const found for xtal " << detId.rawId() << "! something wrong with EcalIntercalibConstants in your DB? ";
+  }
+  icalconst = thisconst;
 }
